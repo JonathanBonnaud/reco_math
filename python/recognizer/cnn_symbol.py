bbecf89e1aa7cb8b74842c5c859e245812ce7be0
Kevin ESPASA <kevin.espasa@etu.univ-nantes.fr>
2018-01-29 19:37:56 +0100

    remplacement tensorflow par lasagne

--- python/recognizer/cnn_symbol.py
+++ python/recognizer/cnn_symbol.py
@@ -1,124 +1,81 @@
-#  Copyright 2016 The TensorFlow Authors. All Rights Reserved.
-#
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
-"""Convolutional Neural Network Estimator for MNIST, built with tf.layers."""
-
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-
 from sklearn.preprocessing import LabelBinarizer
 import numpy as np
-import tensorflow as tf
+import theano
+import theano.tensor as T
+
+import lasagne
 from pathlib import Path
 from python.code.usefulTools import load_images
-
-tf.logging.set_verbosity(tf.logging.INFO)
-
+import time
 
 
-def cnn_model_fn(features, labels, mode):
-  """Model function for CNN."""
-  # Input Layer
-  # Reshape X to 4-D tensor: [batch_size, width, height, channels]
-  # MNIST images are 28x28 pixels, and have one color channel
-  input_layer = tf.reshape(features["x"], [-1, 28, 28, 1])
+def build_cnn(input_var=None):
+  # As a third model, we'll create a CNN of two convolution + pooling stages
+  # and a fully-connected hidden layer in front of the output layer.
 
-  # Convolutional Layer #1
-  # Computes 32 features using a 5x5 filter with ReLU activation.
-  # Padding is added to preserve width and height.
-  # Input Tensor Shape: [batch_size, 28, 28, 1]
-  # Output Tensor Shape: [batch_size, 28, 28, 32]
-  conv1 = tf.layers.conv2d(
-      inputs=input_layer,
-      filters=32,
-      kernel_size=[5, 5],
-      padding="same",
-      activation=tf.nn.relu)
+  # Input layer, as usual:
+  network = lasagne.layers.InputLayer(shape=(None, 1, 28, 28),
+                                      input_var=input_var)
+  # This time we do not apply input dropout, as it tends to work less well
+  # for convolutional layers.
 
-  # Pooling Layer #1
-  # First max pooling layer with a 2x2 filter and stride of 2
-  # Input Tensor Shape: [batch_size, 28, 28, 32]
-  # Output Tensor Shape: [batch_size, 14, 14, 32]
-  pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
+  # Convolutional layer with 32 kernels of size 5x5. Strided and padded
+  # convolutions are supported as well; see the docstring.
+  network = lasagne.layers.Conv2DLayer(
+          network, num_filters=32, filter_size=(5, 5),
+          nonlinearity=lasagne.nonlinearities.rectify,
+          W=lasagne.init.GlorotUniform())
+  # Expert note: Lasagne provides alternative convolutional layers that
+  # override Theano's choice of which implementation to use; for details
+  # please see http://lasagne.readthedocs.org/en/latest/user/tutorial.html.
 
-  # Convolutional Layer #2
-  # Computes 64 features using a 5x5 filter.
-  # Padding is added to preserve width and height.
-  # Input Tensor Shape: [batch_size, 14, 14, 32]
-  # Output Tensor Shape: [batch_size, 14, 14, 64]
-  conv2 = tf.layers.conv2d(
-      inputs=pool1,
-      filters=64,
-      kernel_size=[5, 5],
-      padding="same",
-      activation=tf.nn.relu)
+  # Max-pooling layer of factor 2 in both dimensions:
+  network = lasagne.layers.MaxPool2DLayer(network, pool_size=(2, 2))
 
-  # Pooling Layer #2
-  # Second max pooling layer with a 2x2 filter and stride of 2
-  # Input Tensor Shape: [batch_size, 14, 14, 64]
-  # Output Tensor Shape: [batch_size, 7, 7, 64]
-  pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
+  # Another convolution with 32 5x5 kernels, and another 2x2 pooling:
+  network = lasagne.layers.Conv2DLayer(
+          network, num_filters=32, filter_size=(5, 5),
+          nonlinearity=lasagne.nonlinearities.rectify)
+  network = lasagne.layers.MaxPool2DLayer(network, pool_size=(2, 2))
 
-  # Flatten tensor into a batch of vectors
-  # Input Tensor Shape: [batch_size, 7, 7, 64]
-  # Output Tensor Shape: [batch_size, 7 * 7 * 64]
-  pool2_flat = tf.reshape(pool2, [-1, 7 * 7 * 64])
+  # A fully-connected layer of 256 units with 50% dropout on its inputs:
+  network = lasagne.layers.DenseLayer(
+          lasagne.layers.dropout(network, p=.5),
+          num_units=256,
+          nonlinearity=lasagne.nonlinearities.rectify)
 
-  # Dense Layer
-  # Densely connected layer with 1024 neurons
-  # Input Tensor Shape: [batch_size, 7 * 7 * 64]
-  # Output Tensor Shape: [batch_size, 1024]
-  dense = tf.layers.dense(inputs=pool2_flat, units=1024, activation=tf.nn.relu)
+  # And, finally, the 10-unit output layer with 50% dropout on its inputs:
+  network = lasagne.layers.DenseLayer(
+          lasagne.layers.dropout(network, p=.5),
+          num_units=102,
+          nonlinearity=lasagne.nonlinearities.softmax)
 
-  # Add dropout operation; 0.6 probability that element will be kept
-  dropout = tf.layers.dropout(
-      inputs=dense, rate=0.4, training=mode == tf.estimator.ModeKeys.TRAIN)
+  return network
 
-  # Logits layer
-  # Input Tensor Shape: [batch_size, 1024]
-  # Output Tensor Shape: [batch_size, 10]
-  logits = tf.layers.dense(inputs=dropout, units=102)
 
-  predictions = {
-      # Generate predictions (for PREDICT and EVAL mode)
-      "classes": tf.argmax(input=logits, axis=1),
-      # Add `softmax_tensor` to the graph. It is used for PREDICT and by the
-      # `logging_hook`.
-      "probabilities": tf.nn.softmax(logits, name="softmax_tensor")
-  }
-  if mode == tf.estimator.ModeKeys.PREDICT:
-    return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
+# ############################# Batch iterator ###############################
+# This is just a simple helper function iterating over training data in
+# mini-batches of a particular size, optionally in random order. It assumes
+# data is available as numpy arrays. For big datasets, you could load numpy
+# arrays as memory-mapped files (np.load(..., mmap_mode='r')), or write your
+# own custom data iteration function. For small datasets, you can also copy
+# them to GPU at once for slightly improved performance. This would involve
+# several changes in the main program, though, and is not demonstrated here.
+# Notice that this function returns only mini-batches of size `batchsize`.
+# If the size of the data is not a multiple of `batchsize`, it will not
+# return the last (remaining) mini-batch.
 
-  # Calculate Loss (for both TRAIN and EVAL modes)
-  loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
-
-  # Configure the Training Op (for TRAIN mode)
-  if mode == tf.estimator.ModeKeys.TRAIN:
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
-    train_op = optimizer.minimize(
-        loss=loss,
-        global_step=tf.train.get_global_step())
-    return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
-
-  # Add evaluation metrics (for EVAL mode)
-  eval_metric_ops = {
-      "accuracy": tf.metrics.accuracy(
-          labels=labels, predictions=predictions["classes"])}
-  return tf.estimator.EstimatorSpec(
-      mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
-
+def iterate_minibatches(inputs, targets, batchsize, shuffle=False):
+  assert len(inputs) == len(targets)
+  if shuffle:
+      indices = np.arange(len(inputs))
+      np.random.shuffle(indices)
+  for start_idx in range(0, len(inputs) - batchsize + 1, batchsize):
+      if shuffle:
+          excerpt = indices[start_idx:start_idx + batchsize]
+      else:
+          excerpt = slice(start_idx, start_idx + batchsize)
+      yield inputs[excerpt], targets[excerpt]
 
 def load_label(path):
   file = open(path,"r")
@@ -137,61 +94,86 @@ def main():
 
   symbol_labels = load_label("data/DataSymbol_Iso/task2-trainSymb2014/trainingSymbols/iso_GT.txt")
   junk_labels = load_label("data/DataSymbol_Iso/task2-trainSymb2014/trainingJunk/junk_GT.txt")
-  print(len(junk_labels)/2)
   train_labels = symbol_labels + junk_labels[:74284]
   train_labels = np.asarray(train_labels)  
-  train_labels = encoder.fit_transform(train_labels)
+  y_train = encoder.fit_transform(train_labels)
   #num_classes = len(set(train_labels)) 
   if Path("train_data.npy").exists():
-    train_data = np.load("train_data.npy")
+    X_train = np.load("train_data.npy")
   else: 
-    train_data = load_images("data/DataSymbol_Iso/data/train")
-    np.save("train_data", train_data)
+    X_train = load_images("data/DataSymbol_Iso/data/train")
+    np.save("train_data", X_train)
   
   eval_labels = np.asarray(load_label("data/DataSymbol_Iso/task2-testSymbols2014/testSymbols_2016_iso_GT.txt"))
-  eval_labels = encoder.fit_transform(eval_labels)
+  y_test = encoder.fit_transform(eval_labels)
   if Path("eval_data.npy").exists():
-    eval_data = np.load("eval_data.npy")
+    y_test = np.load("eval_data.npy")
   else:
-    eval_data = load_images("data/DataSymbol_Iso/data/test")
-    np.save("eval_data", eval_data)
-  """
-  # Load training and eval data
-  mnist = tf.contrib.learn.datasets.load_dataset("mnist")
-  train_data = mnist.train.images  # Returns np.array
-  train_labels = np.asarray(mnist.train.labels, dtype=np.int32)
-  eval_data = mnist.test.images  # Returns np.array
-  eval_labels = np.asarray(mnist.test.labels, dtype=np.int32)
-  """
-  # Create the Estimator
-  mnist_classifier = tf.estimator.Estimator(
-      model_fn=cnn_model_fn, model_dir="/tmp/mnist_convnet_model")
+    y_test = load_images("data/DataSymbol_Iso/data/test")
+    np.save("eval_data", y_test)
 
-  # Set up logging for predictions
-  # Log the values in the "Softmax" tensor with label "probabilities"
-  tensors_to_log = {"probabilities": "softmax_tensor"}
-  logging_hook = tf.train.LoggingTensorHook(
-      tensors=tensors_to_log, every_n_iter=50)
+  input_var = T.tensor4('inputs')
+  target_var = T.ivector('targets')
 
-  # Train the model
-  train_input_fn = tf.estimator.inputs.numpy_input_fn(
-      x={"x": train_data},
-      y=train_labels,
-      batch_size=100,
-      num_epochs=None,
-      shuffle=True)
-  mnist_classifier.train(
-      input_fn=train_input_fn,
-      steps=100,
-      hooks=[logging_hook])
+  num_epochs=500
 
-  # Evaluate the model and print results
-  eval_input_fn = tf.estimator.inputs.numpy_input_fn(
-      x={"x": eval_data},
-      y=eval_labels,
-      num_epochs=1,
-      shuffle=False)
-  eval_results = mnist_classifier.evaluate(input_fn=eval_input_fn)
+  network = build_cnn(input_var)
 
-  print(eval_results)
-  
+# Create a loss expression for training, i.e., a scalar objective we want
+  # to minimize (for our multi-class problem, it is the cross-entropy loss):
+  prediction = lasagne.layers.get_output(network)
+  loss = lasagne.objectives.categorical_crossentropy(prediction, target_var)
+  loss = loss.mean()
+  # We could add some weight decay as well here, see lasagne.regularization.
+
+  # Create update expressions for training, i.e., how to modify the
+  # parameters at each training step. Here, we'll use Stochastic Gradient
+  # Descent (SGD) with Nesterov momentum, but Lasagne offers plenty more.
+  params = lasagne.layers.get_all_params(network, trainable=True)
+  updates = lasagne.updates.nesterov_momentum(
+          loss, params, learning_rate=0.01, momentum=0.9)
+
+  # Create a loss expression for validation/testing. The crucial difference
+  # here is that we do a deterministic forward pass through the network,
+  # disabling dropout layers.
+  test_prediction = lasagne.layers.get_output(network, deterministic=True)
+  test_loss = lasagne.objectives.categorical_crossentropy(test_prediction,
+                                                          target_var)
+  test_loss = test_loss.mean()
+  # As a bonus, also create an expression for the classification accuracy:
+  test_acc = T.mean(T.eq(T.argmax(test_prediction, axis=1), target_var),
+                    dtype=theano.config.floatX)
+
+  # Compile a function performing a training step on a mini-batch (by giving
+  # the updates dictionary) and returning the corresponding training loss:
+  train_fn = theano.function([input_var, target_var], loss, updates=updates, allow_input_downcast=True)
+
+  # Compile a second function computing the validation loss and accuracy:
+  val_fn = theano.function([input_var, target_var], [test_loss, test_acc])
+
+  # Finally, launch the training loop.
+  print("Starting training...")
+  # We iterate over epochs:
+  for epoch in range(num_epochs):
+      # In each epoch, we do a full pass over the training data:
+      train_err = 0
+      train_batches = 0
+      start_time = time.time()
+      for batch in iterate_minibatches(X_train, y_train, 500, shuffle=True):
+          inputs, targets = batch
+          train_err += train_fn(inputs, targets)
+          train_batches += 1
+  # After training, we compute and print the test error:
+  test_err = 0
+  test_acc = 0
+  test_batches = 0
+  for batch in iterate_minibatches(X_test, y_test, 500, shuffle=False):
+      inputs, targets = batch
+      err, acc = val_fn(inputs, targets)
+      test_err += err
+      test_acc += acc
+      test_batches += 1
+  print("Final results:")
+  print("  test loss:\t\t\t{:.6f}".format(test_err / test_batches))
+  print("  test accuracy:\t\t{:.2f} %".format(
+      test_acc / test_batches * 100))
